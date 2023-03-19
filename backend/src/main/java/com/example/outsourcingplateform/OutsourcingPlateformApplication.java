d08974ce8e1eec1868a1de5d75f5738e0e8a2d18
stormsidali2001 <assoulsidali@gmail.com>
2023-03-19 14:52:43 +0100

    creating the eureka discovery server , registring the worker in the eureka server

---
+++ backend/src/main/java/com/example/outsourcingplateform/OutsourcingPlateformApplication.java
@@ -0,0 +1,13 @@
+package com.example.outsourcingplateform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsourcingPlateformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutsourcingPlateformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/outsourcingplateform/OutsourcingPlateformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.outsourcingplateform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutsourcingPlateformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
