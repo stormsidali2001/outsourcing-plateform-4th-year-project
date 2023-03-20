d88cd1f0064ed0e4384e289ee2b3f801f294bd65
stormsidali2001 <assoulsidali@gmail.com>
2023-03-20 12:13:48 +0100

    creating s signuprequest endpoint

--- backend/worker-microservice/src/main/java/com/example/workermicroservice/controller/WorkerController.java
+++ backend/worker-microservice/src/main/java/com/example/workermicroservice/controller/WorkerController.java
@@ -1,6 +1,9 @@
 package com.example.workermicroservice.controller;
 
 import com.example.workermicroservice.dto.signupRequestDto.SignUpRequestDto;
+import com.example.workermicroservice.service.WorkerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api")
 public class WorkerController {
 
-    
+    @Autowired
+    private WorkerService workerService;
 
-    public void signupRequest(SignUpRequestDto signUpRequestDto){
-
-
+    @GetMapping("sign-up-request")
+    public String signupRequest(SignUpRequestDto signUpRequestDto){
+         workerService.signUpWorker(signUpRequestDto);
+         return "done";
     }
 }
--- backend/worker-microservice/src/main/java/com/example/workermicroservice/service/WorkerService.java
+++ backend/worker-microservice/src/main/java/com/example/workermicroservice/service/WorkerService.java
@@ -1,12 +1,35 @@
 package com.example.workermicroservice.service;
 
+import com.example.workermicroservice.Entities.worker.EducationDetail;
+import com.example.workermicroservice.Entities.worker.Skill;
+import com.example.workermicroservice.Entities.worker.Worker;
+import com.example.workermicroservice.dto.signupRequestDto.EducationDetailDto;
+import com.example.workermicroservice.dto.signupRequestDto.SignUpRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 public class WorkerService {
 
-    public void signUpWorker(){
-
+    public void signUpWorker(SignUpRequestDto sq){
+        Worker worker = Worker.builder()
+                .firstName(sq.getFirstName())
+                .lastName(sq.getLastName())
+                .educationDetails(sq.getEducationDetails())
+                .build();
     }
+
+    public EducationDetail mapEducationDetail(EducationDetailDto ed){
+        return EducationDetail.builder()
+                .description(ed.getDescription())
+                .field(ed.getField())
+                .startDate(ed.getStartDate())
+                .endDate(ed.getEndDate())
+                .school(ed.getSchool())
+                .location(ed.getLocation())
+                .skills()
+                .build();
+    }
+
+    public Skill mapToSkill(S)
 }
