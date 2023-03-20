02d3700092c3389ca5f1d84269ba084563cabd42
stormsidali2001 <assoulsidali@gmail.com>
2023-03-20 12:07:25 +0100

    updating the signup request dto and the related dtos

--- backend/worker-microservice/src/main/java/com/example/workermicroservice/controller/WorkerController.java
+++ backend/worker-microservice/src/main/java/com/example/workermicroservice/controller/WorkerController.java
@@ -1,9 +1,17 @@
 package com.example.workermicroservice.controller;
 
+import com.example.workermicroservice.dto.signupRequestDto.SignUpRequestDto;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
 public class WorkerController {
+
+    
+
+    public void signupRequest(SignUpRequestDto signUpRequestDto){
+
+
+    }
 }
