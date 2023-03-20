5b5b62544f5d4594cec40ba69a03e43776c8a5e5
stormsidali2001 <assoulsidali@gmail.com>
2023-03-20 12:55:17 +0100

    validating skill

--- backend/worker-microservice/src/main/java/com/example/workermicroservice/dto/signupRequestDto/SkillDto.java
+++ backend/worker-microservice/src/main/java/com/example/workermicroservice/dto/signupRequestDto/SkillDto.java
@@ -3,10 +3,15 @@ package com.example.workermicroservice.dto.signupRequestDto;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Builder @Data
 public class SkillDto {
+
+    @NotBlank(message = "skill name is required")
     private String name;
 
+    @NotBlank(message = "skill category is required")
     private String category;
 
 }
