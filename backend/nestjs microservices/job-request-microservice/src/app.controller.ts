066dcd636687f38d059347a11c81114ab239b6e2
stormsidali2001 <assoulsidali@gmail.com>
2023-05-18 16:19:34 +0200

    worker fixes

--- backend/nestjs microservices/job-request-microservice/src/app.controller.ts
+++ backend/nestjs microservices/job-request-microservice/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get , Post} from '@nestjs/common';
 import { AppService } from './app.service';
+import { JobRequestDto } from './dto/create-job-request.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+
+  @Post()
+  async create(@Body() data:JobRequestDto){
+    return this.appService.create(data);
   }
 }
--- backend/nestjs microservices/job-request-microservice/src/app.module.ts
+++ backend/nestjs microservices/job-request-microservice/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { EurekaModule } from 'nestjs-eureka';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JobRequest, JobRequestSchema } from './schemas/job-request.schema';
 @Module({
-  imports: [],
+  imports: [
+    EurekaModule.forRoot({
+      eureka: {
+        host: 'localhost',
+      port: 8888,
+      registryFetchInterval: 1000,
+      servicePath: '/eureka/apps/',
+      maxRetries: 3,
+      },
+      service: {
+        name: 'job-request-service',
+        port: 5006,
+      },
+    }),
+
+    MongooseModule.forRoot("mongodb://localhost:27017/job-requests-db"),
+    MongooseModule.forFeature([{name:JobRequest.name,schema:JobRequestSchema}])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
