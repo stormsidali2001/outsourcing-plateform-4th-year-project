2edcb13e4050c91396ceede163d9d18377ca7d22
unknown <s.beloufa@esi-sba.dz>
2023-06-06 13:02:02 -0700

    gateway-cors-config

---
+++ backend/springboot microservices/api-gateway/src/main/java/com/example/apigateway/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.example.apigateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig
+        extends CorsConfiguration
+{
+    @Bean
+    public CorsWebFilter corsFilter()
+    {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials( true );
+        config.setAllowedOrigins( List.of( "http://localhost:4200" ) );
+        config.setAllowedMethods( List.of( "GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD" ) );
+        config.setAllowedHeaders( List.of( "origin", "content-type", "accept", "authorization", "cookie" ) );
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration( "/**", config );
+
+        return new CorsWebFilter( source );
+    }
+}
--- backend/springboot microservices/auth-microservice/src/main/java/com/example/authmicroservice/controller/AuthController.java
+++ backend/springboot microservices/auth-microservice/src/main/java/com/example/authmicroservice/controller/AuthController.java
@@ -23,6 +23,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+
 public class AuthController {
 
 
